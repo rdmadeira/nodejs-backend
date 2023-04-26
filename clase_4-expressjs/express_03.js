c76847cda9c8e191e48769e59ea443f0a472a8ad
rdmadeira <rdmadeira2@gmail.com>
2023-04-26 17:34:35 -0300

    Add .env file

--- clase_4-expressjs/express_03.js
+++ clase_4-expressjs/express_03.js
@@ -1,12 +1,17 @@
 import express from 'express';
+//install dotenv:
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 //app.use() corre para todas las rutas:
 app.use(express.static('public'));
 
 app.all('/*'); // Para cualquier ruta, se direcciona a la carpeta public, o sea, nuestro frontend.
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000...');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}...`);
 });
