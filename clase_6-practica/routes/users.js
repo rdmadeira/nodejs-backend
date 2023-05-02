1699b8266d8fb102b93dd5986dfde4c4a8d6db5f
rdmadeira <rdmadeira2@gmail.com>
2023-05-01 22:28:47 -0300

    users change handlers, went out of index

--- clase_6-practica/routes/users.js
+++ clase_6-practica/routes/users.js
@@ -1,15 +1,42 @@
 import express from 'express';
 import path from 'path';
 import { __dirname } from '../paths.js';
-import { getUsers } from '../utils/utils.js';
+import { getUsers, saveUsers } from '../utils/utils.js';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/pages/users.html'));
+  const users = getUsers();
+  res.status(200).json({
+    data: users,
+    message: 'Successfully fetched Users',
+    status: 'success',
+  });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', (req, res) => {
+  const newUser = { ...req.body, id: uuidv4() };
+  const users = getUsers();
+
+  if (!users.find((user) => user.email === newUser.email)) {
+    users.push(newUser);
+    saveUsers(users);
+    res.status(200).json({
+      message: 'User registered Successfully',
+      data: newUser,
+      status: 'success',
+    });
+  } else {
+    res.status(202).json({
+      message: 'Email already registered!',
+      status: 'error',
+      data: null,
+    });
+  }
+});
+
+router.post('/user', (req, res, next) => {
   const userEmail = req.body.email;
   const users = getUsers();
   const user = users.find((us) => us.email === userEmail);
@@ -20,7 +47,7 @@ router.post('/', (req, res, next) => {
       .json({ message: 'User finded!', data: user, status: 'success' });
   } else {
     res
-      .status(204)
+      .status(202)
       .json({ message: 'No user finded', data: null, status: 'error' });
   }
 });
